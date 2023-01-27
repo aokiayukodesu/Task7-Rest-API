7fdf3b0f4adb326ee663e7c5e06d1ed5927da3e2
aokiayukodesu <catch.you.later1031@gmail.com>
2023-01-27 22:14:05 +0900

    NamesControllerにクラス名を修正

--- src/main/java/com/example/task7/NamesController.java
+++ src/main/java/com/example/task7/NamesController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-public class namesController {
+public class NamesController {
     //クライアントから/names宛の[nameのリストをください]というGetリクエストがサーバーに飛んできて、
     //GetMappingでリクエストを受け取り、リストを返す。クライアントとcontrollerだけのやり取り・・という認識で良いのでしょうか？
     @GetMapping("/names")
-    public List <String> getName(){
+    public List<String> getName() {
         return List.of("koyama","tanaka");
     }
 }
